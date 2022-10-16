e6b00acf6bc8a7258ab4ab979f92b415fe9be614
Deyan Bozhilov <dodobrat98@gmail.com>
2022-10-16 12:11:46 +0300

    triger browser open for regular dev run + disable it for docker run

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [svelte()],
-	envPrefix: "SVELTE_APP",
-	server: {
-		host: true,
-		strictPort: true,
-		port: 3000,
-		watch: {
-			usePolling: true,
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), "");
+
+	const baseConfig = {
+		plugins: [svelte()],
+		envPrefix: "SVELTE_APP",
+		server: {
+			host: true,
+			strictPort: true,
+			port: 3000,
 		},
-	},
+	};
+
+	if (env.SVELTE_APP_DOCKER_RUN === "true") {
+		return baseConfig;
+	}
+	return {
+		...baseConfig,
+		server: {
+			...baseConfig.server,
+			open: "/",
+		},
+	};
 });
